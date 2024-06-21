dcdcf354e81956680c134d92874374fcabf2f879
Julien Prugne <julien@webeleon.dev>
2024-06-21 11:12:49 +0200

    chore: setup graphql apollo

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <h1 class="text-3xl font-bold underline">
-    Hello world!
+    {{JSON.stringify(data)}}
   </h1>
 </template>
+
+<script setup lang="ts">
+import allTravelQuery from './travel/query/all-travel.gql'
+
+const { data } = useAsyncQuery(allTravelQuery)
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,21 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  modules: ['@nuxtjs/apollo'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: 'http://localhost:5000/graphql' // in real app load from env var or a config vault
+      }
     },
   },
 })
